59436d6ae641a6e9f091ff1add6c22faa5d308b3
Joseph Korang <jkorang@gmail.com>
2015-04-08 22:05:03 -0400

    Initial Commit

---
+++ Agrivolution/Controls.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title=""        Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Controls.aspx.cs" Inherits="Agrivolution.MCUSearch" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2><%: Title %></h2>
+</asp:Content>
---
+++ Agrivolution/Controls.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace Agrivolution
+{
+    public partial class MCUSearch : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Title = "Placeholder";
+        }
+
+    }
+}
