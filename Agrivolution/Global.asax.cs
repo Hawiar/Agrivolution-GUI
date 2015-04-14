087881f0b3e467dd02870d03f75c83688e097f1c
Hawiar <Hawiar6@gmail.com>
2015-04-13 23:40:46 -0400

    Removed authentication code and fixed an email issue

--- Agrivolution/Global.asax.cs
+++ Agrivolution/Global.asax.cs
@@ -19,28 +19,28 @@ namespace Agrivolution
         }
 
         //Should check if user is logged in, otherwise redirect them to login page
-        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
-        {
-            // This is the page
-            string cTheFile = HttpContext.Current.Request.Path;
+        //protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        //{
+        //    // This is the page
+        //    string cTheFile = HttpContext.Current.Request.Path;
 
-            // Check if I am all ready on login page to avoid crash
-            if (!cTheFile.EndsWith("login.aspx"))
-            {
-                // Extract the form's authentication cookie
-                string cookieName = FormsAuthentication.FormsCookieName;
-                HttpCookie authCookie = Context.Request.Cookies[cookieName];
+        //    // Check if I am all ready on login page to avoid crash
+        //    if (!cTheFile.EndsWith("login.aspx") || !cTheFile.EndsWith("register.aspx"))
+        //    {
+        //        // Extract the form's authentication cookie
+        //        string cookieName = FormsAuthentication.FormsCookieName;
+        //        HttpCookie authCookie = Context.Request.Cookies[cookieName];
 
-                // If not logged in
-                //if (null == authCookie)
-                // Alternative way of checking:
-                if (HttpContext.Current.User == null || HttpContext.Current.User.Identity == null || !HttpContext.Current.User.Identity.IsAuthenticated)
-                {
-                    Response.Redirect("/login.aspx", true);
-                    Response.End();
-                    return;
-                }
-            }
-        }
+        //        // If not logged in
+        //        if (null == authCookie)
+        //        // Alternative way of checking:
+        //        //if (HttpContext.Current.User == null || HttpContext.Current.User.Identity == null || !HttpContext.Current.User.Identity.IsAuthenticated)
+        //        {
+        //            Response.Redirect("/login.aspx", true);
+        //            Response.End();
+        //            return;
+        //        }
+        //    }
+        //}
     }
 }
